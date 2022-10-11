a0991d123d6de8c26ad74ece5278eb0cc001e2ee
toannc <nguyencongtoan14051997@gmail.com>
2022-10-11 23:43:29 +0700

    main

--- section2/src/main/java/ssv/section2/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package ssv.section2.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ssv.section2.domain.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-
-}
